69e9932712c6c2f7baa3ef902992ca0e9bc73408
Evin Revello <evin.revello@gmail.com>
2021-02-11 19:20:18 -0800

    resolving issue with non-sorted glob ordering

--- save_latest_image.py
+++ save_latest_image.py
@@ -8,19 +8,17 @@ import time
 
 def saveLastImage():
     #  TODO: Make this more extensible
-    allJpegs = glob.glob("/home/" + os.environ['MACHINE_NAME'] + "/Pictures/*.jpeg")
+    allJpegs = sorted(glob.glob("/home/" + os.environ['MACHINE_NAME'] + "/Pictures/*.jpeg"))
     command = "cp /home/" + os.environ['MACHINE_NAME'] + "/latest.jpeg /home/" + os.environ['MACHINE_NAME'] + "/Pictures/"
   
     if len(allJpegs) == 0:
-        # this assumes no other images are in the directory
-        print('the list is empty')
         # TODO: os.system is deprecated, you should use the subprocessmodule instead.
         command = command + "1.jpeg"
         os.system(command)
         # this command is not executing   
         time.sleep(3)
         return None   
-    lastFileName = allJpegs[0]
+    lastFileName = allJpegs[len(allJpegs)-1]
     parsedNumer = re.search(r'(?<=.)\d+', lastFileName)
     newNumber = str(int(parsedNumer.group(0)) + 1)
     command = command + newNumber + ".jpeg"
